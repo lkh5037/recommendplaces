ab32c06e72eb79f929328be539a369222276c7f6
lkh5037 <sciencestudywork@gmail.com>
2026-04-18 10:11:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_recommendations(data, region, indoor):
     return result
 
 st.title("강원 청소년 생활 도우미")
-selected_region = st.selectbox("지역을 선택하세요", ["강릉", "속초", "춘천"])
+selected_region = st.selectbox("지역을 선택하세요", ["강릉", "속초", "춘천","양양"])
 selected_indoor = st.radio("실내 여부를 선택하세요", ["실내", "실외"])
 
 if st.button("추천 보기"):
